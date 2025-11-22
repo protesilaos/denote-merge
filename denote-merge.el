27ecd2494bfd9b1f8d0aaf9c6254a08cb8ea768c
Protesilaos Stavrou <info@protesilaos.com>
2025-11-22 07:30:39 +0200

    Expand the package Commentary

--- denote-merge.el
+++ denote-merge.el
@@ -25,9 +25,11 @@
 
 ;;; Commentary:
 ;;
-;; WORK-IN-PROGRESS.
-;;
-;; Optional Denote extension to merge contents from one note into another.
+;; This is an optional extension to the `denote' package.  It provides
+;; commands and relevant user options to streamline the work of
+;; merging contents from one Denote file to another.  This is for
+;; users who periodically review their notes to add, remove, or
+;; otherwise consolidate their accumulated knowledge.
 
 ;;; Code:
 
